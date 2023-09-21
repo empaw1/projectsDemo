7acd9d754136aa5b7c6840acac9de54acc9cc16d
empaw1 <52018243+empaw1@users.noreply.github.com>
2023-09-20 20:20:47 -0400

    Update redditImageGrab.py

--- redditPython/redditImageGrab.py
+++ redditPython/redditImageGrab.py
@@ -23,7 +23,7 @@ import pyautogui
 import time
 import pyperclip as clip
 
-#integrating sorting features specific to vehicles. will then takes selected parameters and create cookie cutter type short segment videos
+
 
 CLIENT_ID = '4mCP6cVA6Tj91htrOS0c8A'
 SECRET_KEY = 'i2uy2-QAQOfDuNb2e7x1CNUGA'
